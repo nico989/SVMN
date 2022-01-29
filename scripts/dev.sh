04dc08cff4567a030dbb305dd58ec0757bebb84e
Carlo Corradini <carlo.ita98@gmail.com>
2022-01-29 15:16:36 +0100

    Fix for better sync

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,7 +4,7 @@
 __DIRNAME="$(dirname "$( realpath "${BASH_SOURCE[0]}" )" )"
 readonly __DIRNAME
 # Watch directory
-readonly WATCH_DIR="${__DIRNAME}/../src"
+readonly WATCH_DIR="${__DIRNAME}/../src/"
 # Destination directory
 readonly DESTINATION_DIR="${__DIRNAME}/../comnetsemu/app/morphing_slices"
 
@@ -16,9 +16,13 @@ source "${__DIRNAME}/__commons.sh"
 assert_tool inotifywait
 assert_tool rsync
 
+# Initialize working directory
+INFO "Initializing working directory"
+rsync -avz "$WATCH_DIR" "$DESTINATION_DIR"
+
 # Watcher
 INFO "Starting watcher";
 while inotifywait -r -e modify,create,delete,move "$WATCH_DIR"; do
     rsync -avz "$WATCH_DIR" "$DESTINATION_DIR"
 done
-INFO "Stopping watcher";
+INFO "Watcher stopped";
