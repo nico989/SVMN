266a45a0342219bbf9264cafe703ad7d6ea4fc79
Carlo Corradini <carlo.ita98@gmail.com>
2022-01-29 17:01:56 +0100

    dev script (rsync) now update also when a file/folder is deleted

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -6,7 +6,7 @@ readonly __DIRNAME
 # Watch directory
 readonly WATCH_DIR="${__DIRNAME}/../src/"
 # Destination directory
-readonly DESTINATION_DIR="${__DIRNAME}/../comnetsemu/app/morphing_slices"
+readonly DEST_DIR="${__DIRNAME}/../comnetsemu/app/morphing_slices/"
 
 # Include commons
 # shellcheck source=__commons.sh
@@ -16,13 +16,19 @@ source "${__DIRNAME}/__commons.sh"
 assert_tool inotifywait
 assert_tool rsync
 
-# Initialize working directory
-INFO "Initializing working directory"
-rsync -avz "$WATCH_DIR" "$DESTINATION_DIR"
+# Sync watch and destination directories
+sync() {
+    DEBUG "Syncing '$WATCH_DIR' with destination '$DEST_DIR'"
+    rsync --archive --verbose --compress --delete --human-readable "$WATCH_DIR" "$DEST_DIR"
+}
+
+# Sync directory
+INFO "Syncing directory '$WATCH_DIR'"
+sync
 
 # Watcher
-INFO "Starting watcher";
+INFO "Starting watcher on '$WATCH_DIR' with destination '$DEST_DIR'";
 while inotifywait -r -e modify,create,delete,move "$WATCH_DIR"; do
-    rsync -avz "$WATCH_DIR" "$DESTINATION_DIR"
+    sync
 done
 INFO "Watcher stopped";
