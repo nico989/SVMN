d59dbd4bfcdb639f7e19d56d89c3fb805cf7558d
nico989 <vincinicolo98@gmail.com>
2022-02-01 12:17:31 +0100

    Refromat client.py

--- src/client.py
+++ src/client.py
@@ -1,15 +1,47 @@
+#!/usr/bin/env python3
+
 import socket
 import time
+import argparse
 
-SERVICE_IP = "10.0.0.12"
-SERVICE_PORT = 8888
 
-if __name__ == "__main__":
+def runClient(ip, port, timeout):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     data = b"Show me the counter, please!"
-
     while True:
-        sock.sendto(data, (SERVICE_IP, SERVICE_PORT))
+        sock.sendto(data, (ip, port))
         counter, _ = sock.recvfrom(1024)
         print("Current counter: {}".format(counter.decode("utf-8")))
-        time.sleep(1)
+        time.sleep(timeout)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Simple client pinging server",
+        formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=40),
+    )
+    parser.add_argument(
+        "-i", "--ip", help="Server IP", required=True, action="store", type=str
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        help="Server Port",
+        required=True,
+        action="store",
+        type=int,
+    )
+    parser.add_argument(
+        "-t",
+        "--timeout",
+        help="Interval time to send data",
+        required=True,
+        action="store",
+        type=int,
+    )
+    args = parser.parse_args()
+    runClient(args.ip, args.port, args.timeout)
+
+
+if __name__ == "__main__":
+    main()
